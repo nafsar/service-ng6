89d25bec8240ffaa08c19d9237895f169587e1f1
nafsar <n.afsar@gmail.com>
2018-05-09 22:12:41 -0600

    angular 6 service

--- src/app/member.service.ts
+++ src/app/member.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map, catchError } from 'rxjs/operators';
-import { observable, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Mem } from './mem';
 
 @Injectable({
